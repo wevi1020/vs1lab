eefc9a42e8ac2a7bd9a3a9e55fc7e18837149533
krma1080 <krma1080@h-ka.de>
2024-12-08 18:36:40 +0100

    Update geotagging.js

--- Aufgabe3/gta_v3/public/javascripts/geotagging.js
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -17,46 +17,7 @@ console.log("The geoTagging script is going to start...");
 /**
  * A class to help using the Leaflet map service.
  */
-class MapManager {
 
-    #map
-    #markers
-
-    /**
-    * Initialize a Leaflet map
-    * @param {number} latitude The map center latitude
-    * @param {number} longitude The map center longitude
-    * @param {number} zoom The map zoom, defaults to 18
-    */
-    initMap(latitude, longitude, zoom = 18) {
-        // set up dynamic Leaflet map
-        this.#map = L.map('map').setView([latitude, longitude], zoom);
-        var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer(
-            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; ' + mapLink + ' Contributors'}).addTo(this.#map);
-        this.#markers = L.layerGroup().addTo(this.#map);
-    }
-
-    /**
-    * Update the Markers of a Leaflet map
-    * @param {number} latitude The map center latitude
-    * @param {number} longitude The map center longitude
-    * @param {{latitude, longitude, name}[]} tags The map tags, defaults to just the current location
-    */
-    updateMarkers(latitude, longitude, tags = []) {
-        // delete all markers
-        this.#markers.clearLayers();
-        L.marker([latitude, longitude])
-            .bindPopup("Your Location")
-            .addTo(this.#markers);
-        for (const tag of tags) {
-            L.marker([tag.latitude,tag.longitude])
-                .bindPopup(tag.name)
-                .addTo(this.#markers);  
-        }
-    }
-}
 
 /**
  * TODO: 'updateLocation'
